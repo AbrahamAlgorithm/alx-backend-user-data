cd8d7b50e6bbf21d7478d320fcd2444603386b68
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-18 23:19:04 +0100

    Hash Password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+"""Authentication module"""
+
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+from typing import Union
 
 def _hash_password(Password: str) -> bytes:
     """Hash Password"""
@@ -6,3 +14,9 @@ def _hash_password(Password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     return hash
+
+def _generate_uuid() -> str:
+    '''
+    generate uuid
+    '''
+    return str(uuid4())
