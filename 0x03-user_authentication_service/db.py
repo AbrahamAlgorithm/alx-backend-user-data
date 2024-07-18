45691125316e421b6a46c9e990a0878948667f2d
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-18 23:00:49 +0100

    Update User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,3 +49,12 @@ class DB:
                 if getattr(user, k) == v:
                     return user
         raise NoResultFound
+    
+    def update_user(self, user_id, **kwargs):
+        """Update a user in user"""
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in User.__dict__:
+                raise ValueError
+            user.k = v
+        self._session.commit()
