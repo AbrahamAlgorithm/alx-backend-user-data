de38af5b21141d9daaa5ad29d7ff417675b710bc
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-18 23:35:44 +0100

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,6 +55,23 @@ def logout():
     abort(403)
     
     
+@app.route("/profile", method=["GET"], strict_slashes=False)
+def profile() -> str:
+    """
+    get profile
+    Return:
+        message if found
+    else:
+        abort
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+    
+    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
