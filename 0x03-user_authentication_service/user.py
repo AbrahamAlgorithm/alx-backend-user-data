19c99455a6464bcb271274c932f6b42714d1c5cf
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-18 11:17:51 +0100

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,12 +5,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     "SqlAlchemy User table for User model"
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable==False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
