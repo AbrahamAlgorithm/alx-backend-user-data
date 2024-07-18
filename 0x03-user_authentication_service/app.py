1e67cb2dcd68d35f9cc533cd05adcbfe5afc6cd5
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-18 23:36:04 +0100

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,6 +71,19 @@ def profile() -> str:
     abort(403)
     
     
+@app.route("/reset_password", method=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    get reset password token
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 
 
 if __name__ == "__main__":
