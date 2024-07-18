abc71ed3bf2af0f0356d06741e49e3fd11a56093
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-18 23:34:42 +0100

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -13,6 +13,34 @@ def index():
     """GEt the indx page"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    '''
+    return all users
+    '''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    
+    
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    '''
+    login user
+    '''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    cookies = AUTH.create_session(email)
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie("session_id", cookies)
+    return resp
+
 
 
 if __name__ == "__main__":
